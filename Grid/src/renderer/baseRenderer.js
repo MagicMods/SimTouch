2fa261591ccd6177185470133531c20845ed342a
MagicMods <contact@magicmods.net>
2025-04-29 20:08:47 +0100

    CleanUp - Remove Grid

--- Grid/src/renderer/baseRenderer.js
+++
@@ -1,8 +0,0 @@
-export class BaseRenderer {
-  constructor(gl, shaderManager) {
-    if (!gl) throw new Error("WebGL context is required.");
-    if (!shaderManager) throw new Error("ShaderManager instance is required.");
-    this.gl = gl;
-    this.shaderManager = shaderManager;
-  }
-}
